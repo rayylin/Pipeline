2afb0216f4295bb17908da47f225f5dec8e5d27e
rayl <dwadeflashback@gmail.com>
2025-04-23 11:56:03 -0400

    update structure

--- postgresql.py
+++ postgresql.py
@@ -1,43 +1,44 @@
 import psycopg2
 
-from pipeline import fetch_data
-
-
-# Database connection parameters
-conn = psycopg2.connect(
-    host="localhost",
-    port=5432,
-    dbname="mydb",
-    user="ray",
-    password="abc"
-)
-
-cur = conn.cursor()
-
-# Create table
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS people (
-        id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL
-    );
-""")
-
-# Insert sample data
-cur.execute("INSERT INTO people (name) VALUES (%s), (%s)", ("Alice", "Bob"))
-
-# Query data
-cur.execute("SELECT * FROM people")
-rows = cur.fetchall()
-
-print("Data in 'people' table:")
-for row in rows:
-    print(row)
-
-# Cleanup
-conn.commit()
-cur.close()
-conn.close()
 
 
 
-df = fetch_data()
+
+
+# from pipeline import fetch_data
+
+def execSql(query):
+    pass
+
+    # Database connection parameters
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        dbname="mydb",
+        user="ray",
+        password="abc"
+    )
+    cur = conn.cursor()
+
+
+    # Create table
+    cur.execute()
+
+    # Insert sample data
+    # cur.execute("INSERT INTO people (name) VALUES (%s), (%s)", ("Alice", "Bob"))
+
+    # Query data
+    cur.execute("SELECT * FROM cashflow")
+    rows = cur.fetchall()
+
+    print("Data in 'people' table:")
+    for row in rows:
+        print(row)
+
+    # Cleanup
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+
