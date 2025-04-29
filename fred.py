bcb86ea4c85a50fd2d3fd7e1e7b0ae04b4c8c274
rayl <dwadeflashback@gmail.com>
2025-04-28 23:25:04 -0400

    add more data from fred

--- fred.py
+++ fred.py
@@ -4,3 +4,14 @@ from config import fredApi
 fred = Fred(api_key= fredApi)
 rate = fred.get_series('FEDFUNDS')
 print(rate.tail())
+
+l = ["UNRATE"  # Unemployment rate
+     ,"PAYEMS" # Nonfarm Payrolls
+     ,'FEDFUNDS']
+
+for i in l:
+    rate = fred.get_series(i)
+    print(rate.tail())
+    print(rate.shape[0])
+
+
