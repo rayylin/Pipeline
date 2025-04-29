96531137f9fdbc5d8a3ca32d15a46c18fca009e4
rayl <dwadeflashback@gmail.com>
2025-04-29 00:05:26 -0400

    update fred

--- fred.py
+++ fred.py
@@ -5,9 +5,28 @@ fred = Fred(api_key= fredApi)
 rate = fred.get_series('FEDFUNDS')
 print(rate.tail())
 
-l = ["UNRATE"  # Unemployment rate
-     ,"PAYEMS" # Nonfarm Payrolls
-     ,'FEDFUNDS']
+l = [# Interest Rates
+     'FEDFUNDS' # Federal Funds Rate
+     ,"MORTGAGE30US" # Mortgage rates
+     ,"GS10"    # 10-Year Treasury
+
+     # Inflation & Prices
+     ,"CPIAUCSL" # CPI (Consumer Price Index)
+     ,"PCE"      # PCE
+     ,"PPIACO"   # Producer Price Index
+
+     # Employment & Labor
+     ,"UNRATE"    # Unemployment rate
+     ,"PAYEMS"    # Nonfarm Payrolls
+
+     # GDP & Growth	
+     , "GDP" # Real GDP
+     , "GDPC1" # Real Gross Domestic Product
+
+     # Housing 
+     ,"HOUST"     # New Housing Starts
+     ,"CSUSHPISA" #Home Price Index
+     ]
 
 for i in l:
     rate = fred.get_series(i)
