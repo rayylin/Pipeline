ce907ec2eb3e630018cff92946e3a88dd7522467
rayl <dwadeflashback@gmail.com>
2025-05-19 23:32:45 -0400

    tmp

--- yahoofinance.py
+++ yahoofinance.py
@@ -5,17 +5,28 @@ ticker = 'AAPL'
 stock = yf.Ticker(ticker)
 
 # Fetch the cash flow statement
-cashflow = stock.cashflow
+# cashflow = stock.cashflow
 
-# Show the cash flow statement
-print("Cash Flow Statement:")
-print(cashflow)
+# # Show the cash flow statement
+# print("Cash Flow Statement:")
+# print(cashflow)
 
 
 
-cashflow_transposed = cashflow.transpose()
+# cashflow_transposed = cashflow.transpose()
 
-print("\nTransposed Cash Flow Statement:")
-print(cashflow_transposed)
+# print("\nTransposed Cash Flow Statement:")
+# print(cashflow_transposed)
 
-print(cashflow_transposed.columns.values)
+# print(cashflow_transposed.columns.values)
+
+info = stock.info
+# Print selected info
+print("Company Name:", info.get("longName"))
+print("Industry:", info.get("industry"))
+print("Sector:", info.get("sector"))
+print("Website:", info.get("website"))
+print("Country:", info.get("country"))
+print("Full-time Employees:", info.get("fullTimeEmployees"))
+print("Summary:", info.get("longBusinessSummary"))
+print("Summary:", info.get("calendar"))
