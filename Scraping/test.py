5c8b9eecf2e826412accec4f28e4f5e43ef0b34d
rayl <dwadeflashback@gmail.com>
2025-07-05 15:03:37 -0400

    can get title

--- Scraping/test.py
+++ Scraping/test.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.sina.com.cn/roll/index.d.html?cid=57038&page=1"
+url = "https://finance.sina.cn/2025-07-05/detail-infemuii8216637.d.html?vt=4"
+#https://finance.sina.cn/roll.d.html?rollCid=57038"
+# https://finance.sina.com.cn/roll/index.d.html?cid=57038&page=1"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36'
 }
@@ -10,7 +12,7 @@ response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'lxml')
 
 # Example: find main headlines in <a> tags inside <div class="blk_hd">
-headlines = soup.select('a')  # You can refine this with actual structure
+headlines = soup.select('title')  # You can refine this with actual structure
 
 print("=== Headline Examples ===")
 for a in headlines[:10]:  # Print top 10
