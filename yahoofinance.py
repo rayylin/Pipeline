a44116516ad812872637388536f1104abc68e4bc
rayl <dwadeflashback@gmail.com>
2025-07-04 23:03:31 -0400

    rate limit

--- yahoofinance.py
+++ yahoofinance.py
@@ -21,7 +21,7 @@ for i in range(3):
         print("Summary:", info.get("calendar"))
     except:
         print("Rate Limit")
-        time.sleep(4)
+        time.sleep(6)
 
 
 # Fetch the cash flow statement
