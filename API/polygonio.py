8d5c3607fb8bee2e9613a83fb4cac90cff5db33f
rayl <dwadeflashback@gmail.com>
2025-07-13 23:51:59 -0400

    tmp

--- API/polygonio.py
+++ API/polygonio.py
@@ -8,6 +8,38 @@ from config import polygonapi
 import requests
 from config import polygonapi  # or use os.getenv() if using .env
 
+from polygon import RESTClient
+import datetime
+
+
+
+def GetLatestDaily(ticker:str):
+
+
+    client = RESTClient(polygonapi)
+
+    today = LastOpenDate = datetime.datetime.now()
+    weekday = today.strftime("%w")
+
+    if weekday in ("0","6"): # Saturdat, Sunday
+        LastOpenDate  = today +  datetime.timedelta(days=-2) if weekday == 0 else today +  datetime.timedelta(days=-1) # if weekday in ("0","6") else today
+
+    request = client.get_daily_open_close_agg(
+        ticker,
+        f"{LastOpenDate.strftime("%Y-%m-%d")}",
+        adjusted="true",
+    )
+
+    return request
+
+if __name__ ==  '__main__':
+    tick = "AAPL"
+    req = GetLatestDaily(tick)
+
+    print(f"close:{req.close} : , high:{req.high} : , low:{req.low} : , open:{req.open} : , volume:{req.volume} : ,")
+
+    
+
 # Sample stock ticker
 ticker = "AAPL"
 
@@ -29,40 +61,40 @@ else:
 
 
 
-from polygon import RESTClient
-from polygon.rest.models import (
-    TickerNews,
-)
+# from polygon import RESTClient
+# from polygon.rest.models import (
+#     TickerNews,
+# )
 
 
-client = RESTClient(polygonapi)
+# client = RESTClient(polygonapi)
 
-ticker = "AAPL"
+# ticker = "AAPL"
 
-# List Aggregates (Bars)
-aggs = []
-for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_="2023-01-01", to="2023-06-13", limit=50000):
-    aggs.append(a)
+# # List Aggregates (Bars)
+# aggs = []
+# for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_="2023-01-01", to="2023-06-13", limit=50000):
+#     aggs.append(a)
 
-print(aggs)
+# print(aggs)
 
-# Get Last Trade
-trade = client.get_last_trade(ticker=ticker)
-print(trade)
+# # Get Last Trade
+# trade = client.get_last_trade(ticker=ticker)
+# print(trade)
 
-# List Trades
-trades = client.list_trades(ticker=ticker, timestamp="2022-01-04")
-for trade in trades:
-    print(trade)
+# # List Trades
+# trades = client.list_trades(ticker=ticker, timestamp="2022-01-04")
+# for trade in trades:
+#     print(trade)
 
-# Get Last Quote
-quote = client.get_last_quote(ticker=ticker)
-print(quote)
+# # Get Last Quote
+# quote = client.get_last_quote(ticker=ticker)
+# print(quote)
 
-# List Quotes
-quotes = client.list_quotes(ticker=ticker, timestamp="2022-01-04")
-for quote in quotes:
-    print(quote)
+# # List Quotes
+# quotes = client.list_quotes(ticker=ticker, timestamp="2022-01-04")
+# for quote in quotes:
+#     print(quote)
 
 
 
