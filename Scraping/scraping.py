f1448ce28b876aec8a350881637070e33c818d2a
rayl <dwadeflashback@gmail.com>
2025-07-23 22:52:06 -0400

    test qcc

--- Scraping/scraping.py
+++ Scraping/scraping.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.sina.com.cn/"
+url = r"https://top.qcc.com/"#https://finance.sina.com.cn/"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36'
 }
