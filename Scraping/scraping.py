e072247042537a316a2aa394300b4ce2e65c0cec
rayl <dwadeflashback@gmail.com>
2025-07-23 23:08:23 -0400

    tmp

--- Scraping/scraping.py
+++ Scraping/scraping.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = r"https://top.qcc.com/"#https://finance.sina.com.cn/"
+url = r"https://data.gcis.nat.gov.tw/od/data/api/6BBA2268-1367-4B42-9CCA-BC17499EBE8C?$format=xml&$filter=Company_Name like 台灣積體電路製造股份有限公司 and Company_Status eq 01&$skip=0&$top=50"#https://top.qcc.com/"#https://finance.sina.com.cn/"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36'
 }
