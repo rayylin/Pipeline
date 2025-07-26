4ff6e29d95cc11c1c60c3cc66c86ab8c3732ed8a
rayl <dwadeflashback@gmail.com>
2025-07-25 23:23:12 -0400

    HK api can work

---
+++ Scraping/HkCmpInfo.py
@@ -0,0 +1,21 @@
+import requests
+
+
+url = "https://data.cr.gov.hk/cr/api/api/v1/api_builder/json/local/search"
+
+# Define the query parameters
+params = {
+    "query[0][key1]": "Comp_name",
+    "query[0][key2]": "begins_with",
+    "query[0][key3]": "hsbc"
+}
+
+# Make the GET request
+response = requests.get(url, params=params)
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
