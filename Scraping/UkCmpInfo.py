5388ff28e5c1c89171b1346f5a37bbb9666fbec2
rayl <dwadeflashback@gmail.com>
2025-07-25 23:20:24 -0400

    tmp

--- Scraping/UkCmpInfo.py
+++ Scraping/UkCmpInfo.py
@@ -5,7 +5,7 @@ from requests.auth import HTTPBasicAuth
 api_key = 'YOUR_API_KEY'
 company_number = '02723534'
 
-url = f'https://www.qcckyc.com/enterprise-details?' #https://api.company-information.service.gov.uk/company/{company_number}'
+url = f'https://find-and-update.company-information.service.gov.uk/company/04366849/officers' #https://api.company-information.service.gov.uk/company/{company_number}'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36'
 }
