be82d20432b0248aa3740825900445bd20111051
rayl <dwadeflashback@gmail.com>
2025-08-04 22:36:12 -0400

    restructure

---
+++ mongo/cmpInfo.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
+
+
+def getTwCmpInfo(Cmp:str):
+    url = f"https://data.gcis.nat.gov.tw/od/data/api/6BBA2268-1367-4B42-9CCA-BC17499EBE8C?$format=xml&$filter=Company_Name like {Cmp} and Company_Status eq 01&$skip=0&$top=50"#https://top.qcc.com/"#https://finance.sina.com.cn/"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36'
+    }
+
+    response = requests.get(url, headers=headers)
+    xml_str = response.text
+
+    return xml_str
--- mongo/mongo.py
+++ mongo/mongo.py
@@ -1,30 +1,34 @@
 from pymongo import MongoClient
+from cmpInfo import getTwCmpInfo
 
 # Connect to MongoDB (localhost:27017)
 client = MongoClient("mongodb://localhost:27017/")
 
 # Access (or create) database and collection
-db = client["test_database"]
-collection = db["test_collection"]
+db = client["sai"]
+collection = db["cmp_collection"]
 
 # Insert a document
-insert_result = collection.insert_one({"name": "Alice", "age": 30})
-print("Inserted ID:", insert_result.inserted_id)
+# insert_result = collection.insert_one({"name": "Cecilia", "age": 30})
+# print("Inserted ID:", insert_result.inserted_id)
+
+s = getTwCmpInfo("台灣積體電路製造股份有限公司")
+
 
 # Find one document
-document = collection.find_one({"name": "Alice"})
+document = collection.find_one({"name": "Cecilia"})
 print("Found document:", document)
 
-# Update a document
-collection.update_one({"name": "Alice"}, {"$set": {"age": 31}})
+# # Update a document
+# collection.update_one({"name": "Alice"}, {"$set": {"age": 31}})
 
-# Find updated document
-updated = collection.find_one({"name": "Alice"})
-print("Updated document:", updated)
+# # Find updated document
+# updated = collection.find_one({"name": "Alice"})
+# print("Updated document:", updated)
 
-# Delete document
-collection.delete_one({"name": "Alice"})
+# # Delete document
+# collection.delete_one({"name": "Alice"})
 
-# Confirm deletion
-deleted = collection.find_one({"name": "Alice"})
-print("After deletion:", deleted)
+# # Confirm deletion
+# deleted = collection.find_one({"name": "Alice"})
+# print("After deletion:", deleted)
