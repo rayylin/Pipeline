7381a3c64efa03c8fa10e1d35fb5dccf9abcd459
rayl <dwadeflashback@gmail.com>
2025-12-16 00:46:56 -0500

    update class

--- mongo/searchAndFilter.py
+++ mongo/searchAndFilter.py
@@ -1,18 +1,32 @@
 from pymongo import MongoClient
 import re
 from config import mongoUri
+from enum import Enum
 
+
+class Choice(Enum):
+    HK = ("Company_HK_Original", ["Business Name", "Business Name(Chinese)", "brNo"])
+    TW = ("Company_TW_Original", ["統一編號", "公司名稱"])
+    # UK = "" future dev
+    # CN = ""
+
+
+def parse_choice(user_input: str) -> Choice:
+    try:
+        return Choice[user_input.upper()]
+    except KeyError:
+        raise ValueError("Invalid choice. Must be HK or TW")
+
+
+source = "HK"
+collectionName = parse_choice(source)
 
 
 client = MongoClient(mongoUri)
-db = client["test_db"]
-collection = db["Company_HK_Orginal"]
+db = client["company_db"]
+collection = db[collectionName.value[0]]
 
 def search_business(keyword: str, limit: int = 5):
-    """
-    Search 'Business Name' and 'Business Name(Chinese)' for the keyword.
-    Returns up to `limit` matching documents.
-    """
     # Escape keyword so special regex chars (., *, ?, etc.) don't break the search
     pattern = re.escape(keyword)
 
@@ -32,5 +46,5 @@ for doc in search_business("Ae"):
     print(doc.get("Business Name"), "|", doc.get("Business Name(Chinese)"))
 
 # Search by Chinese keyword
-for doc in search_business("航"):
+for doc in search_business("光"):
     print(doc.get("Business Name"), "|", doc.get("Business Name(Chinese)"))
